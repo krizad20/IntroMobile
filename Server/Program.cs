b060c72a6987af57a60b73608d1956eab9513e93
krizad20 <vim_intason@hotmail.com>
2022-10-19 16:07:13 +0700

    edit .net Program

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,19 +1,71 @@
-namespace ToDo
+using Microsoft.IdentityModel.Tokens;
+
+// dotnet add package Microsoft.AspNetCore.Authentication.JwtBearer
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(options =>
 {
-    public class Program
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
     {
-        public static string SecurityKey = "abcdefgh12345678";
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = "public",
+        ValidIssuer = "todo",
+        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(Program.SecurityKey))
+    };
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseUrls("http://localhost:5000").UseStartup<Startup>();
-                });
-    }
+//set port 5000
+builder.WebHost.UseUrls("http://localhost:5000");
 
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors(Options =>
+{
+    Options.AllowAnyOrigin();
+    Options.AllowAnyHeader();
+    Options.AllowAnyMethod();
+}
+);
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+
+
+app.Run();
+
+public partial class Program
+{
+
+    // random string (length = 16)
+    public static string SecurityKey = "abcdefgh12345678";
 }
